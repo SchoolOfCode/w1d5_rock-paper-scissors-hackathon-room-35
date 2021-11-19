efeee886fb4562a22d6ba7837a2a16c9c6ca6e2c
ingasafranova <sfrnv@yahoo.co.uk>
2021-11-19 12:19:14 +0000

    new lines

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 
-let playerMove = prompt("Whats your move?");
+
+let playerMove ='rock';
 
 
+let computerMove = 'paper';
 
+if( playerMove === 'rock' && computerMove ==='paper' )
+{ console.log('paper wins')} 
+
+
+else if( playerMove==='rock' && computerMove==='sissors')
+{console.log('rock wins')}
+ else if ( playerMove === 'rock' && computerMove ==='paper')
+ { console.log('paper wins')};
+
+
+function getWinner(player1, player2) {
+    // code goes here...
+  }
+  
+  let result = getWinner("rock", "paper");
